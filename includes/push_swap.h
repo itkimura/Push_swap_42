699b863a3c46e5be589f0e2588e32e823fba03f8
Itoe Kimura <itkimura@student.hive.fi>
2022-08-18 17:10:53 +0300

    Add functions

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -10,12 +10,20 @@
 
 typedef	struct		s_dlst
 {
-	long			value;
+	int			*value;
 	struct s_dlst	*next;
 	struct s_dlst	*prev;
 }					t_dlst;
 
+/* push_swap.h */
+int	error(void);
 
 /* validate_args.c */
-int validate_args(int ac, char **av);
+int	validate_args(int ac, char **av);
+
+/* init_numbers */
+int	init_numbers(int ac, char **av, int **numbers, int *total);
+
+/* init_stack */
+int	init_stack(t_dlst **stack_a, t_dlst **stack_b, int *numbers, int total);
 #endif
